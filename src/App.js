dbb603a4d8a2119443eee6ba515ccda256c57097
LeoKrasowski <leokrasowski@gmail.com>
2024-05-23 19:02:00 +0200

    Added functionality to show description for each items

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Items from "./components/Items";
 import Categores from "./components/Categores";
+import ShowFullItem from "./components/ShowFullItem";
 
 class App extends React.Component {
   constructor(props) {
@@ -76,23 +77,30 @@ class App extends React.Component {
           "price": "19.99"
         },
 
-      ]
+      ],
+      showFullItem: false
     }
     this.state.currentItems = this.state.items
     this.addToOrder = this.addToOrder.bind(this)
     this.deleteOrder = this.deleteOrder.bind(this)
     this.chooseCategory = this.chooseCategory.bind(this)
+    this.onShowItem = this.onShowItem.bind(this)
   }
   render() {
     return (
       <div className="wrapper">
         <Header orders={this.state.orders} onDelete={this.deleteOrder} />
         <Categores chooseCategory={this.chooseCategory} />
-        <Items items={this.state.currentItems} onAdd={this.addToOrder} />
+        <Items onShowItem={this.onShowItem} items={this.state.currentItems} onAdd={this.addToOrder} />
+
+        {this.state.showFullItem && <ShowFullItem />}
         <Footer />
       </div>
+    )
+  }
 
-    );
+  onShowItem() {
+    this.setState({ showFullItem: !this.state.showFullItem })
   }
 
   chooseCategory(category) {
---
+++ src/components/ShowFullItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ShowFullItem extends Component {
+    render() {
+        return (
+            <div>
+                jop tvoju mat!
+            </div>
+        )
+    }
+}
+
+export default ShowFullItem
